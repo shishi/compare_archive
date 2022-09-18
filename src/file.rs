d798653d70f59430c4a49ad425c8327f7e9396fa
Shigenobu Nishikawa <shishi@srevo.net>
2022-09-19 02:26:18 +0900

    read zip archive

--- src/file.rs
+++ src/file.rs
@@ -1,9 +1,21 @@
 use anyhow::Result;
 
 #[derive(Debug)]
-struct ZipFile {}
+pub struct ZipFile {}
 
-async fn read_zip_file() -> Result<ZipFile> {
+pub async fn read_zip_file() -> Result<ZipFile> {
+    // it will receive file path as args and loop
     let mut zip = zip::ZipArchive::new(std::fs::File::open("foo.zip")?)?;
+
+    for i in 0..zip.len() {
+        let mut file = zip.by_index(i)?;
+        println!(
+            "File {} has size {} and is compressed to {} bytes and CRC32 is {}",
+            file.name(),
+            file.size(),
+            file.compressed_size(),
+            format!("{:x}", file.crc32())
+        );
+    }
     return Ok(ZipFile {});
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+use compare_archive::file;
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
+    file::read_zip_file().await;
 }
+
+// use std::env;
+//
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
+//
+//     let query = &args[1];
+//     let filename = &args[2];
+//
+//     // {}を探しています
+//     println!("Searching for {}", query);
+//     // {}というファイルの中
+//     println!("In file {}", filename);
+// /$ cargo run test sample.txt
+//    Compiling minigrep v0.1.0 (file:///projects/minigrep)
+//     Finished dev [unoptimized + debuginfo] target(s) in 0.0 secs
+//      Running `target/debug/minigrep test sample.txt`
+// Searching for test
+// In file sample.txt
+// / }
